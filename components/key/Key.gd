87bd9f12a6b8d814aeab2de15025a372bb186212
to-jajav33 <to.jajav33@gmail.com>
2019-12-21 13:40:07 -0800

    working on level1

---
+++ components/key/Key.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+export var keyID = 1;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
