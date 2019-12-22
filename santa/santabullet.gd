2c65aa396a4671483f205bd18fa648cbd3f5dd19
John Watson <john@watson-net.com>
2019-12-21 21:22:28 -0800

    Explosions

--- santa/santabullet.gd
+++ santa/santabullet.gd
@@ -1,6 +1,10 @@
 extends RigidBody2D
 
+const Explosion = preload("res://components/explosion/explosion.tscn")
+
+
 func _ready():
+	angular_velocity = rand_range(-4*PI, 4*PI)
 	pass
 
 
@@ -11,5 +15,9 @@ func _on_VisibilityNotifier2D_screen_exited():
 func _on_SantaBullet_body_entered(body):
 	if body.has_method("hurt"):
 		body.hurt()
-		
+	
+	var e = Explosion.instance()
+	e.global_position = global_position
+	get_tree().current_scene.add_child(e)
+	
 	queue_free()
