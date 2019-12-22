0deb76da9e697df7d5a8c2dbc28d82d82090564a
John Watson <john@watson-net.com>
2019-12-21 20:56:56 -0800

    Elf animations

--- boss/Boss.gd
+++ boss/Boss.gd
@@ -1,10 +1,10 @@
 extends KinematicBody2D
 
-var alive = true
-var health = 1000
-var SPEED = 300
+export var health : int = 100
+export var SPEED : int = 200
 
-export var state : String
+var state : String
+var alive = true
 
 
 func _ready():
@@ -15,8 +15,8 @@ func hurt(amount : int = 1):
 	if not alive:
 		return
 		
-	$Tween.interpolate_property($Sprite, 'modulate', Color(1,1,1,1), Color(10,1,1,1), 0.01, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
-	$Tween.interpolate_property($Sprite, 'modulate', Color(10,1,1,1), Color(1,1,1,1), 0.01, Tween.TRANS_LINEAR, Tween.EASE_IN, 0.01)
+	$Tween.interpolate_property($Sprite, 'modulate', Color(1,1,1,1), Color(10,10,10,1), 0.01, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+	$Tween.interpolate_property($Sprite, 'modulate', Color(10,10,10,1), Color(1,1,1,1), 0.01, Tween.TRANS_LINEAR, Tween.EASE_IN, 0.01)
 	$Tween.start()
 	
 	health -= amount
@@ -36,17 +36,36 @@ func _physics_process(delta):
 	var player = get_tree().get_nodes_in_group("player")[0]
 	
 	if state == "idle":
+		$Sprite.play("idle")
 		pass
 		
 	if state == "awake":
 		pass
 		
 	if state == "chase":
+		$Sprite.play("walk")
+			
+	if state == "attack":
+		$Sprite.play("attack")
+		
+	if state == "chase" or state == "attack":
 		var direction = global_position.direction_to(player.global_position)
 		move_and_slide(direction * SPEED)
 		
-		
+		if direction.x < 0 and not $Sprite.flip_h:
+			$Sprite.flip_h = true
+			
+		if direction.x > 0 and $Sprite.flip_h:
+			$Sprite.flip_h = false
+			
+	for i in range(get_slide_count()):
+		var collision = get_slide_collision(i)
+		if collision.collider.has_method("hurt"):
+			$AnimationPlayer.play("attack")
+			$AnimationPlayer.queue("chase")
+			collision.collider.hurt()
+
+
 func change_state():
 	state = $AnimationPlayer.current_animation
-	print(state)
 	
