509ee0d01ffec87a6f74f76817fc15c97cd0c90c
John Watson <john@watson-net.com>
2019-12-21 17:29:52 -0800

    Shooting sound

--- santa/santa.gd
+++ santa/santa.gd
@@ -7,6 +7,8 @@ const BULLET_FORCE = 2500
 const Bullet = preload("res://santa/santabullet.tscn")
 onready var gun = $gun
 
+var shoot_sfx = []
+
 
 func _ready():
 	pass
@@ -54,6 +56,8 @@ func _physics_process(delta):
 	if Input.is_action_pressed("shoot") and $shootCooldown.is_stopped():
 		$shootCooldown.start()
 		
+		shoot_sfx()
+		
 		var b = Bullet.instance()
 		get_tree().current_scene.add_child(b)
 		b.global_position = global_position + Vector2(256,0).rotated(gun.rotation)
@@ -64,3 +68,23 @@ func _physics_process(delta):
 			$gun/AnimationPlayer.play("recoil_left")
 		else:
 			$gun/AnimationPlayer.play("recoil_right")
+
+
+func shoot_sfx():
+	$sfxShoot.play()
+	return
+	
+	var found = false
+	for i in range(shoot_sfx.size()):
+		if not shoot_sfx[i].playing:
+			found = true
+			shoot_sfx[i].play()
+			break
+			
+	if not found:
+		var new_sound = $sfxShoot.duplicate()
+		add_child(new_sound)
+		shoot_sfx.append(new_sound)
+		new_sound.play()
+		print(shoot_sfx.size())
+		
